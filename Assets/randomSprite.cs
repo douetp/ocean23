baa60473f3a53eb7e7820ff3606834c4aa7dd997
Hugo <hugojklop52@gmail.com>
2023-03-07 16:32:18 +0100

    Ajout des sprites de déchets

--- Assets/randomSprite.cs
+++ Assets/randomSprite.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class randomSprite : MonoBehaviour
 {
+
+    public Sprite[] sprites;
+    public SpriteRenderer spriteRenderer;
     void Start()
     {
+        //Pick up a random item from the array
+        int randomIndex = Random.Range(0, sprites.Length);
+        //Get the sprite at the random index
+        Sprite randomSprite = sprites[randomIndex];
+        //Set the sprite to the sprite renderer
+        spriteRenderer.sprite = randomSprite;
         
     }
 
